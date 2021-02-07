2b5db77cacaa17f38abe9d734c818809bf218b65
Zdenek Mejzlik <zdenek.mejzlik@gmail.com>
2021-02-07 21:36:52 +0000

    Added start stop buttons

--- lib/arclock_web/live/arclock.ex
+++ lib/arclock_web/live/arclock.ex
@@ -16,12 +16,22 @@ defmodule ArclockWeb.Arclock do
     {:noreply, update(socket, :display, &set_cd/1)}
   end
 
+  def handle_event("start", _, socket) do
+    {:noreply, start(socket)}
+  end
+
+  def handle_event("stop", _, socket) do
+    {:noreply, stop(socket)}
+  end
+
   def render(assigns) do
     ~L"""
     <div>
       <h1><%= @display %>
       <button phx-click="set_ab">Řada AB</button>
       <button phx-click="set_cd">Řada CD</button>
+      <button phx-click="start">Start</button>
+      <button phx-click="stop">Stop</button>
     </div>
     """
   end
@@ -34,5 +44,15 @@ defmodule ArclockWeb.Arclock do
   defp set_cd(_value) do
     Display.set_cd(:display)
     "CD"
+  end
+
+  defp start(socket) do
+    Display.start_countdown(:display, 20)
+    socket
+  end
+
+  defp stop(socket) do
+    Display.stop_countdown(:display)
+    socket
   end
 end
